cde03eca1c8b1328ac674b07ebb4afe1ce40adbb
AntLouiz <luizrodrigo46@hotmail.com>
2020-03-17 09:24:30 -0300

    pagination added to the Attempts view

--- recognitor/api/views.py
+++ recognitor/api/views.py
@@ -1,12 +1,14 @@
 from rest_framework.generics import ListAPIView
 from recognitor.models import Zq1010
 from recognitor.api.serializers import AttemptsModelSerializer
+from recognitor.api.paginations import AttemptsPagination
 
 
 class AttemptsListAPIView(ListAPIView):
 
     model = Zq1010
     serializer_class = AttemptsModelSerializer
+    pagination_class = AttemptsPagination
 
     def get_queryset(self, *args, **kwargs):
         return Zq1010.objects.exclude(d_e_l_e_t_field='*')
