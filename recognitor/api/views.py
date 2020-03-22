d9b917effdb71d5849c9f40023a2dd53143ce517
AntLouiz <luizrodrigo46@hotmail.com>
2020-03-22 08:49:17 -0300

    views refactored with the new recognitor models

--- recognitor/api/views.py
+++ recognitor/api/views.py
@@ -1,14 +1,14 @@
 from rest_framework.generics import ListAPIView
-from recognitor.models import Zq1010
+from recognitor.models import RecognitionAttempt
 from recognitor.api.serializers import AttemptsModelSerializer
 from recognitor.api.paginations import AttemptsPagination
 
 
 class AttemptsListAPIView(ListAPIView):
 
-    model = Zq1010
+    model = RecognitionAttempt
     serializer_class = AttemptsModelSerializer
     pagination_class = AttemptsPagination
 
     def get_queryset(self, *args, **kwargs):
-        return Zq1010.objects.exclude(d_e_l_e_t_field='*')
+        return RecognitionAttempt.objects.all()
