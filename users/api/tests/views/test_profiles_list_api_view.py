5209f83ba82d2302abded648dc184bd7ee76c925
AntLouiz <luizrodrigo46@hotmail.com>
2020-03-25 10:19:21 -0300

    testing the data object bundle on profile list view

--- users/api/tests/views/test_profiles_list_api_view.py
+++ users/api/tests/views/test_profiles_list_api_view.py
@@ -1,12 +1,31 @@
-from django.test import TestCase
+import json
+from rest_framework.test import APITestCase
+from rest_framework.pagination import LimitOffsetPagination
+from model_bakery import baker
+from users.models import UserProfile
+from users.api.serializers import UserProfileSerializer
 
 
-class TestProfilesListAPIView(TestCase):
+class TestProfilesListAPIView(APITestCase):
 
     def setUp(self):
         self.url = '/users/api/'
+        self.users_data = baker.make(UserProfile,  _quantity=10)
 
     def test_get_response_status_code(self):
         response = self.client.get(self.url)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_get_response_object_bundle(self):
+        response = self.client.get(self.url)
+
+        data = response.data
+        data = data['results']
+
+        serializer = UserProfileSerializer(self.users_data, many=True)
+
+        self.assertEqual(
+            data,
+            serializer.data
+        )
